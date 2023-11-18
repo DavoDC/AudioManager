e9a883711929690e524cb13a9fa48040cfb16931
David <45155292+DavoDC@users.noreply.github.com>
2023-11-18 12:56:51 +0800

    Refined Mirror class
    - Refined mirroring stage
    - Switched to XML, saving real path in

---
+++ PROJECT/AudioMirror/Mirror.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AudioMirror
+{
+    /// <summary>
+    /// Creates a lightweight 'mirror' of an Audio track folder
+    /// </summary>
+    internal class Mirror
+    {
+        //// CONSTANTS
+        // Actual Audio folder path
+        string audioFolderPath = @"C:\Users\David\Audio\";
+
+        // Invalid file name characters
+        char[] invalidChars = Path.GetInvalidFileNameChars();
+
+        //// VARIABLES
+        // Variables
+        string mirrorPath;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mirrorPath">The audio mirror folder path</param>
+        public Mirror(string mirrorPath)
+        {
+            // Save start time
+            var startTime = DateTime.Now;
+
+            // Save mirror path
+            this.mirrorPath = mirrorPath;
+
+            // Notify
+            Console.WriteLine($"\nCreating mirror of '{audioFolderPath}'...");
+
+            // Setup folder structure
+            CreateFolders();
+
+            // Populate with files
+            var statisticsInfo = CreateFiles();
+
+            // Calculate execution time
+            var executionTime = DateTime.Now - startTime;
+
+            // Print statistics
+            PrintStats(statisticsInfo, executionTime);
+        }
+
+
+        /// <summary>
+        /// Set up folder structure mirroring the actual folder
+        /// </summary>
+        public void CreateFolders()
+        {
+            // If mirror path is outside of repo
+            if(!mirrorPath.Contains("C:\\Users\\David\\GitHubRepos\\AudioMirror"))
+            {
+                // Throw exception and notify
+                string msg = $"\nMirror path was incorrect, outside the repo:\n{mirrorPath}\n";
+                throw new ArgumentException(msg);
+            }
+
+            // Remove the mirror path if it exists, to recreate it
+            if (Directory.Exists(mirrorPath))
+            {
+                Directory.Delete(mirrorPath, true);
+            }
+
+            // For every actual folder
+            string[] realDirs = Directory.GetDirectories(audioFolderPath, "*", SearchOption.AllDirectories);
+            foreach (var directoryPath in realDirs)
+            {
+                // Get real relative path
+                string relativePath = GetRelativePath(audioFolderPath, directoryPath);
+
+                // Create relative folder within mirror path
+                string newDirectoryPath = Path.Combine(mirrorPath, relativePath);
+                Directory.CreateDirectory(newDirectoryPath);
+            }
+        }
+
+
+        /// <summary>
+        /// Populate folder structure with mirrored files
+        /// </summary>
+        /// <returns>Statistics tuple</returns>
+        private Tuple<int, int, List<string>> CreateFiles()
+        {
+            // Holders
+            int mp3FileCount = 0;
+            int sanitizationCount = 0;
+            List<string> nonMP3Files = new List<string>();
+
+            // For every actual file
+            string[] realFiles = Directory.GetFiles(audioFolderPath, "*", SearchOption.AllDirectories);
+            foreach (var realFilePath in realFiles)
+            {
+                // Get relative file path
+                string relativePath = GetRelativePath(audioFolderPath, realFilePath);
+
+                // For non-MP3 files, add to list and skip
+                if (Path.GetExtension(realFilePath)?.ToLower() != ".mp3")
+                {
+                    nonMP3Files.Add(relativePath);
+                    continue;
+                }
+
+                // Otherwise, process MP3 file
+                if(CreateFile(realFilePath, relativePath))
+                {
+                    sanitizationCount++;
+                }
+
+                mp3FileCount++;
+            }
+
+            // Return holders
+            return Tuple.Create(mp3FileCount, sanitizationCount, nonMP3Files);
+        }
+
+
+        /// <summary>
+        /// Create a mirrored file
+        /// </summary>
+        /// <param name="filePath">The actual file path</param>
+        /// <param name="relativePath">The relative file path</param>
+        /// <returns>True if the filename was sanitized</returns>
+        private bool CreateFile(string realFilePath, string relativePath)
+        {
+            // Sanitization flag
+            bool sanitized = false;
+
+            // Get file name and sanitize it
+            string fileName = Path.GetFileName(realFilePath);
+            string sanitizedFilename = SanitizeFilename(fileName);
+
+            // If file name was sanitized
+            if (fileName != sanitizedFilename)
+            {
+                // Replace filename in path with sanitized version
+                relativePath = relativePath.Replace(fileName, sanitizedFilename);
+
+                // Set flag
+                sanitized = true;
+            }
+
+            // Generate the full mirror path
+            string fullMirrorPath = Path.Combine(mirrorPath, relativePath);
+
+            // Change extension
+            fullMirrorPath = Path.ChangeExtension(fullMirrorPath, ".xml");
+
+            // If the mirrored file doesn't exist already
+            if (!File.Exists(fullMirrorPath))
+            {
+                // Create mirror file containing real path
+                File.WriteAllText(fullMirrorPath, realFilePath);
+            }
+
+            // Return sanitized flag
+            return sanitized;
+        }
+
+
+        /// <summary>
+        /// Print info about completed mirroring process 
+        /// </summary>
+        /// <param name="statisticsInfo"></param>
+        /// <param name="executionTime"></param>
+        private void PrintStats(Tuple<int, int, List<string>> statisticsInfo, TimeSpan executionTime)
+        {
+            // Extract info items
+            int mp3FileCount = statisticsInfo.Item1;
+            int sanitizedFileNames = statisticsInfo.Item2;
+            List<string> nonMP3Files = statisticsInfo.Item3;
+
+            // Print mirror path
+            Console.WriteLine($" - Path: '{mirrorPath}'");
+
+            // Print file count
+            Console.WriteLine($" - MP3 file count: {mp3FileCount}");
+
+            // Print non-MP3 file info
+            Console.WriteLine($" - Non-MP3 files found: {nonMP3Files.Count}");
+            //foreach (var file in nonMP3Files)
+            //{
+            //    Console.WriteLine("   " + file);
+            //}
+
+            // Print sanitization count
+            Console.WriteLine($" - MP3 filenames sanitized: {sanitizedFileNames}");
+
+            // Print time taken
+            Console.WriteLine($" - Time taken: {Math.Round(executionTime.TotalSeconds, 3)} seconds");
+        }
+
+
+        /// <summary>
+        /// Sanitize a given filename of special characters
+        /// </summary>
+        /// <param name="filename">Original filename.</param>
+        /// <returns>Sanitized filename.</returns>
+        private string SanitizeFilename(string filename)
+        {
+            // Remove any non-ASCII characters and replace wide characters with their closest equivalent
+            string sanitizedFilename = new string(filename
+                .Where(c => Char.GetUnicodeCategory(c) != UnicodeCategory.Control &&
+                            Char.GetUnicodeCategory(c) != UnicodeCategory.ModifierSymbol &&
+                            Char.GetUnicodeCategory(c) != UnicodeCategory.OtherSymbol)
+                .ToArray());
+            sanitizedFilename = Encoding.ASCII.GetString(Encoding.GetEncoding("Cyrillic").GetBytes(sanitizedFilename));
+
+            // Replace invalid characters with an underscore if found
+            if (sanitizedFilename.Any(c => invalidChars.Contains(c)))
+            {
+                sanitizedFilename = new string(sanitizedFilename.Select(c => invalidChars.Contains(c) ? '_' : c).ToArray());
+            }
+
+            // Return new file name
+            return sanitizedFilename;
+        }
+
+
+        /// <summary>
+        /// Calculates the relative path from a base path to a target path.
+        /// </summary>
+        /// <param name="basePath">The base path.</param>
+        /// <param name="targetPath">The target path.</param>
+        /// <returns>The relative path from the base path to the target path.</returns>
+        private string GetRelativePath(string basePath, string targetPath)
+        {
+            // Convert paths to URIs for accurate relative path calculation
+            Uri baseUri = new Uri(basePath);
+            Uri targetUri = new Uri(targetPath);
+
+            // Calculate relative URI
+            Uri relativeUri = baseUri.MakeRelativeUri(targetUri);
+
+            // Convert relative URI to a string and unescape special characters
+            return Uri.UnescapeDataString(relativeUri.ToString());
+        }
+    }
+}
--- PROJECT/AudioMirror/Program.cs
+++ PROJECT/AudioMirror/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,31 @@ namespace AudioMirror
 {
     internal class Program
     {
+        /// <summary>
+        /// Main function
+        /// </summary>
+        /// <param name="args">Arguments given to program</param>
         static void Main(string[] args)
         {
-            AudioMirror am = new AudioMirror();
+            // Start message
+            Console.WriteLine("\n###### Audio Mirror ######");
+
+            // Set mirror path relative to program executable
+            string programDir = AppDomain.CurrentDomain.BaseDirectory;
+            string relMirrorPath = "..\\..\\..\\..\\AUDIO_MIRROR";
+            string mirrorPath = Path.GetFullPath(Path.Combine(programDir, relMirrorPath));
+
+            // 1) Create mirror of audio folder
+            Mirror mirror = new Mirror(mirrorPath);
+
+            // 2) Parse metadata into XML files
+            // Parser
+
+            // 3) Analyse metadata
+            // Analyser
+
+            // Finish message
+            Console.WriteLine("\nFinished!\n");
         }
     }
 }
