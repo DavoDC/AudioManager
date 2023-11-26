24bf2c0d834f54dfa375b187fbeb9ece287f7e2e
David <45155292+DavoDC@users.noreply.github.com>
2023-11-26 13:44:42 +0800

    Improve TrackXML class
    - Merged constructors
    - Factored out get/set

--- PROJECT/AudioMirror/Code/Modules/TrackXML.cs
+++ PROJECT/AudioMirror/Code/Modules/TrackXML.cs
@@ -3,25 +3,49 @@ using System.Xml;
 
 namespace AudioMirror.Code.Modules
 {
+    /// <summary>
+    /// Represents a track stored using XML
+    /// </summary>
     internal class TrackXML : Track
+    {
+        // Private fields
+        private XmlDocument xmlDoc;
+        private XmlElement rootElement;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackXML"/> class.
+        /// </summary>
+        /// <param name="mirrorFilePath">The path to the mirror file.</param>
+        /// <param name="tag">The audio metadata. Null if not given</param>
+        public TrackXML(string mirrorFilePath, TrackTag tag = null)
         {
-            //// Private variables
-            private XmlDocument xmlDoc;
-            private XmlElement rootElement;
-
-            /// <summary>
-            /// Read in a track XML object from an EXISTING XML file
-            /// </summary>
-            /// <param name="mirrorFilePath"></param>
-            public TrackXML(string mirrorFilePath)
+            try
             {
-                try
-                {
-                    // Initialize XML document
-                    xmlDoc = new XmlDocument();
-                    xmlDoc.Load(mirrorFilePath);
+                // Initialize XML document
+                xmlDoc = new XmlDocument();
+                rootElement = xmlDoc.CreateElement("Track");
+                xmlDoc.AppendChild(rootElement);
 
-                    // Get the root element
+                // If tag provided
+                if (tag != null)
+                {
+                    // CREATE AN XML FILE
+                    // Set XML elements to metadata values
+                    SetElementValue("Title", tag.Title);
+                    SetElementValue("Artists", tag.Artists);
+                    SetElementValue("Album", tag.Album);
+                    SetElementValue("Year", tag.Year);
+                    SetElementValue("TrackNumber", tag.TrackNumber);
+                    SetElementValue("Genre", tag.Genre);
+                    SetElementValue("Length", tag.Length);
+
+                    // Save file
+                    xmlDoc.Save(mirrorFilePath);
+                }
+                else
+                {
+                    // If no tag, LOAD EXISTING XML FILE
+                    xmlDoc.Load(mirrorFilePath);
                     rootElement = xmlDoc.DocumentElement;
 
                     // Read data from XML and set properties
@@ -33,103 +57,68 @@ namespace AudioMirror.Code.Modules
                     Genre = GetElementValue("Genre");
                     Length = GetElementValue("Length");
                 }
-                catch (Exception ex)
-                {
-                    HandleError("Error occurred while loading EXISTING XML file", mirrorFilePath, ex);
-                }
             }
-
-
-            /// <summary>
-            /// Write out a NEW audio XML file from a track tag
-            /// </summary>
-            /// <param name="mirrorFilePath">The mirror file path</param>
-            /// <param name="tag">The audio metadata</param>
-            public TrackXML(string mirrorFilePath, TrackTag tag)
+            catch (Exception ex)
             {
-                try
-                {
-                    // Initialize XML document
-                    xmlDoc = new XmlDocument();
+                // Create error message
+                string errMsg = "Error occurred while ";
+                errMsg += (tag != null) ? "creating NEW" : "loading EXISTING";
+                errMsg += " XML file!";
 
-                    // Create root element and add
-                    rootElement = xmlDoc.CreateElement("Track");
-                    xmlDoc.AppendChild(rootElement);
-
-                    // Set XML elements to metadata values
-                    SetElement("Title", tag.Title);
-                    SetElement("Artists", tag.Artists);
-                    SetElement("Album", tag.Album);
-                    SetElement("Year", tag.Year);
-                    SetElement("TrackNumber", tag.TrackNumber);
-                    SetElement("Genre", tag.Genre);
-                    SetElement("Length", tag.Length);
-
-                    // Save file
-                    xmlDoc.Save(mirrorFilePath);
-                }
-                catch (Exception ex)
-                {
-                    HandleError("Error occurred while creating NEW XML file!", mirrorFilePath, ex);
-                }
-            }
-
-
-            /// <summary>
-            /// Helper method to get the value of an XML element
-            /// </summary>
-            /// <param name="elementName"></param>
-            /// <returns></returns>
-            private string GetElementValue(string elementName)
-            {
-                var element = rootElement.SelectSingleNode(elementName) as XmlElement;
-                return element?.InnerText ?? string.Empty;
-            }
-
-
-            /// <summary>
-            /// Adds or updates an XML element with the specified name and value.
-            /// </summary>
-            /// <param name="elementName">The name of the XML element.</param>
-            /// <param name="elementValue">The value to set for the XML element.</param>
-            private void SetElement(string elementName, string elementValue)
-            {
-                // Try to retrieve element
-                var existingElement = rootElement.SelectSingleNode(elementName) as XmlElement;
-
-                // If element exists
-                if (existingElement != null)
-                {
-                    // Update existing element and stop
-                    existingElement.InnerText = elementValue;
-                    return;
-                }
-
-                // Otherwise, create a new XML element with the specified name
-                var newElement = xmlDoc.CreateElement(elementName);
-
-                // Set the inner text (value) of the XML element
-                newElement.InnerText = elementValue;
-
-                // Append the new XML element to the root element
-                rootElement.AppendChild(newElement);
-            }
-
-
-            /// <summary>
-            /// Helper method for error handling
-            /// </summary>
-            /// <param name="errorMessage"></param>
-            /// <param name="filePath"></param>
-            /// <param name="ex"></param>
-            private void HandleError(string errorMessage, string filePath, Exception ex)
-            {
-                Console.WriteLine($"\n{errorMessage}");
-                Console.WriteLine($"\nMirror File: {filePath}");
+                // Print info
+                Console.WriteLine($"\n{errMsg}");
+                Console.WriteLine($"\nMirror File: {mirrorFilePath}");
                 Console.WriteLine($"\nError: {ex.Message}");
                 Console.WriteLine($"\nStack Trace: \n{ex.StackTrace}");
                 Console.WriteLine("\n");
                 Environment.Exit(1);
             }
         }
+
+
+        /// <summary>
+        /// Sets the value of the specified XML element.
+        /// </summary>
+        /// <param name="elementName">The name of the XML element.</param>
+        /// <param name="elementValue">The value to set for the XML element.</param>
+        private void SetElementValue(string elementName, string elementValue)
+        {
+            var existingElement = GetXmlElement(elementName);
+
+            // If element exists, set its value
+            if (existingElement != null)
+            {
+                existingElement.InnerText = elementValue;
+                return;
+            }
+
+            // If element does not exist, create a new one
+            var newElement = xmlDoc.CreateElement(elementName);
+            newElement.InnerText = elementValue;
+            rootElement.AppendChild(newElement);
+        }
+
+
+        /// <summary>
+        /// Gets the value of the specified XML element.
+        /// </summary>
+        /// <param name="elementName">The name of the XML element.</param>
+        /// <returns>The value of the XML element, or an empty string if the element is not found.</returns>
+        private string GetElementValue(string elementName)
+        {
+            var existingElement = GetXmlElement(elementName);
+            return existingElement?.InnerText ?? string.Empty;
+        }
+
+
+        /// <summary>
+        /// Gets the specified XML element from the root element.
+        /// </summary>
+        /// <param name="elementName">The name of the XML element.</param>
+        /// <returns>The XML element, or null if not found.</returns>
+        private XmlElement GetXmlElement(string elementName)
+        {
+            return rootElement.SelectSingleNode(elementName) as XmlElement;
+        }
     }
+}
