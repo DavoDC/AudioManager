15020a24bf0b3cb9cecd5c966140b84b80679ca1
David <45155292+DavoDC@users.noreply.github.com>
2024-07-07 09:48:37 +0800

    Added checking of titles for unwanted parts

--- PROJECT/AudioMirror/Code/Doer/LibChecker.cs
+++ PROJECT/AudioMirror/Code/Doer/LibChecker.cs
@@ -1,6 +1,7 @@
 ﻿using AudioMirror.Code.Modules;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AudioMirror
 {
@@ -24,11 +25,56 @@ namespace AudioMirror
             // Save parameter
             this.audioTags = audioTags;
 
-            // TODO
+            // Check titles
+            CheckTitles();
 
             // Print time taken
             Console.WriteLine("");
             PrintTimeTaken();
+        }
+
+        /// <summary>
+        /// Check for unwanted info in titles
+        /// </summary>
+        public void CheckTitles()
+        {
+            Console.WriteLine(" - Checking titles...");
+            var unwantedInfo = new[] { "feat.", "ft.", "edit", 
+                "version", "original", "soundtrack" };
+
+            int totalHits = 0;
+            foreach (TrackTag tag in audioTags)
+            {
+                foreach (var curUnwanted in unwantedInfo)
+                {
+                    totalHits += CheckTitle(tag, curUnwanted);
+                }
+            }
+
+            Console.WriteLine($" - Total hits: {totalHits}");
+        }
+
+        /// <summary>
+        /// Print a message if a given title contains an unwanted substring
+        /// </summary>
+        /// <param name="tag">The track's TrackTag object</param>
+        /// <param name="unwanted">The unwanted parts</param>
+        /// <returns>One if unwanted part was found, zero otherwise</returns>
+        public int CheckTitle(TrackTag tag, params string[] unwanted)
+        {
+            // If tag.Title contains any unwanted element
+            if (unwanted.Any(unwantedS => tag.Title.ToLower().Contains(unwantedS.ToLower())))
+            {
+                // Generate short description of unwanted parts
+                string unwantedDesc = string.Join("/", unwanted);
+
+                // Print message with description and track
+                Console.WriteLine($"  - Found '{unwantedDesc}' in title of '{tag.ToString()}'");
+
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
