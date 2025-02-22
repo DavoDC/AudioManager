bb698433861e60d29d67d4ff7968bf4ca925dd7b
David C <45155292+DavoDC@users.noreply.github.com>
2025-02-22 23:08:04 +0800

    Rebrand as AudioManager
    - This program does a lot more than create the mirror, and the mirror is in a separate repo now

---
+++ PROJECT/AudioManager/Code/Doer/Parser.cs
@@ -0,0 +1,47 @@
+﻿using AudioManager.Code.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AudioManager
+{
+    /// <summary>
+    /// Parses audio track metadata into XML files.
+    /// </summary>
+    internal class Parser : Doer
+    {
+        // Properties
+        public List<TrackTag> audioTags { get; }
+
+        /// <summary>
+        /// Construct a parser
+        /// </summary>
+        /// <param name="mirrorPath">The audio mirror folder path</param>
+        public Parser(string mirrorPath)
+        {
+            // Notify
+            Console.WriteLine("\nParsing audio metadata...");
+
+            // Initialise tag list
+            audioTags = new List<TrackTag>();
+
+            // For every mirrored file
+            string[] mirrorFiles = Directory.GetFiles(mirrorPath, "*", SearchOption.AllDirectories);
+            foreach (var mirrorFilePath in mirrorFiles)
+            {
+                // If non-XML file found, notify
+                if (Path.GetExtension(mirrorFilePath) != ".xml")
+                {
+                    throw new ArgumentException($"Non-XML file found in mirror folder: {mirrorFilePath}");
+                }
+
+                // Get audio file tag and add to list 
+                audioTags.Add(new TrackTag(mirrorFilePath));
+            }
+
+            // Print statistics
+            Console.WriteLine($" - Tags parsed: {audioTags.Count}");
+            PrintTimeTaken();
+        }
+    }
+}
